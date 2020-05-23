f0fd95ca81ecdb8e685682b6b66d44cbfce16c89
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-23 15:39:59 -0500

    Feat: increased difficulty every point.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready( () => {
     console.log("DOM is ready!");
     let score = 0;
+    let time = 2000;
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max) + 1);
     }
@@ -10,13 +11,14 @@ $(document).ready( () => {
 
     const pickAMole = () => {
         let randomNumber = getRandomInt(9);
-        $(`#image-${randomNumber}`).fadeIn(1000).fadeOut();
+        $(`#image-${randomNumber}`).fadeIn(time).fadeOut();
     }
 
-    setInterval(pickAMole, 1000);
+    setInterval(pickAMole, 1500);
 
     $('.images').click( () => {
         score++;
+        time = time-100;
         $('.images').css("display", "none");
         $('#score').html(score);
     });
