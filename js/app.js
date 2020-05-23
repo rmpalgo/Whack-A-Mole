63f5dbb645ab63fa578209c6448e938b73ce722a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-23 15:52:23 -0500

    Feat: winner feature stops the game.

--- js/app.js
+++ js/app.js
@@ -8,19 +8,29 @@ $(document).ready( () => {
 
     console.log(getRandomInt(9));
 
+    const checkIfWin = (score) => {
+        if(score === 19) {
+            let winner = "WINNER!";
+            $('#score').html(winner);
+            clearInterval(interval);
+        }
+    }
 
     const pickAMole = () => {
         let randomNumber = getRandomInt(9);
         $(`#image-${randomNumber}`).fadeIn(time).fadeOut();
     }
 
-    setInterval(pickAMole, 1500);
+   let interval = setInterval(pickAMole, 1500);
 
     $('.images').click( () => {
         score++;
         time = time-100;
         $('.images').css("display", "none");
         $('#score').html(score);
+        checkIfWin(score);
     });
+
+
 
 });
